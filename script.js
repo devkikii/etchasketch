dca39a0ce41356a2c37108fd9a8d6de915fc0732
Kristijan Maric <kiki.maric78@gmail.com>
2025-07-03 23:36:24 +0200

    Added button that prompts user
    Button prompts for a number to be written and also validates it until its correct (Nan, n < 1, n > 100) wont work

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ for (let i = 0; i < 256; i++) {
     cell.style.margin = "1px";
     cell.classList.add("cell")
     container.appendChild(cell)
+    cell.addEventListener("mouseenter", function (e) {
+        cell.style.backgroundColor = "blue"
+    });
 }
+
+let button = document.querySelector("#button");
+button.addEventListener("click", function (e) {
+  let input = prompt("How Many Cells Do You Want");
+  let n = Number(input);
+  while (isNaN(n) || n < 1 || n >= 100) {
+    input = prompt("Please type a valid Number ( > 1, < 100");
+    n = Number(input);
+    console.log(n);
+  }
+});
